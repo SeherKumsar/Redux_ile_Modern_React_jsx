5e23091b0e4f1ebbf6d9c9c9addc0b5f2cf55690
SeherKumsar <seherkumsar99@outlook.com>
2024-03-01 14:40:15 +0300

    Show "Hi there!" on index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // 1) Import the React and ReactDOM libraries
+// 1) React ve ReactDOM kütüphanelerini içeri aktarın
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 // 2) Get a reference to the div with id 'root' in the index.html
+// 2) index.html'deki id'si 'root' olan div'e bir referans alın
+const el = document.getElementById('root');
 
 // 3) Take React to take control of that element and render the App component
+// 3) React'in o elemanı kontrol etmesini ve App bileşenini oluşturmasını sağlayın
+const root = ReactDOM.createRoot(el);
 
 // 4) Create a React component
+// 4) Bir React bileşeni oluşturun
+function App() {
+  return <h1>Hi there!</h1>;
+}
+
+// 5) Show the component on the screen
+// 5) Bileşeni ekranda gösterin
+root.render(<App />);
